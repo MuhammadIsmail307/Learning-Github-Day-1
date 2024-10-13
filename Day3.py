89027030069914813f7779e6672096412b22f077
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-13 19:12:25 +0500

    Day 3
    Day 3

---
+++ Day3.py
@@ -0,0 +1,5 @@
+x = input("Are you want to open any application? ")
+if x = yes
+ print("Click on left mouse button")
+ else
+ print("Don't Click on left mouse button")
---
+++ PythonDay18.py
@@ -0,0 +1,2 @@
+x = input("Enter your name: ")
+print("My Name is", x)
