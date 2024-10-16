f9f41878d2e167d2fad2eacd511effc564de7586
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-16 20:28:48 +0500

    Day6
    Day6

---
+++ Day6.py
@@ -0,0 +1,30 @@
+# Function
+# a = 5
+# b = 7
+# gmean = (a*b)/(a+b)
+# print(gmean)
+# def calculateGmean (a,b):
+#     mean = (a*b)/(a+b)
+
+def calculateGmean (a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+        print("First Number is Greater")
+    else:
+        print("Second Number is Greater or Equal")
+def isLesser(a,b):
+    pass
+a = 50
+b = 7
+c = 45
+d = 87
+e = 46
+f = 90
+isGreater(a,b)
+calculateGmean(a,b)
+isGreater(c,d)
+calculateGmean(c,d)
+isGreater(e,f)
+calculateGmean(e,f)
