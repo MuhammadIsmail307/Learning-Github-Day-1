e2d3128869c56d6c995fb457a8d05464a5b749a6
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-18 15:05:49 +0500

    Create Day8.py

---
+++ Day8.py
@@ -0,0 +1,17 @@
+  # Lists
+marks = [3, 7, 9]
+print(marks)
+print(marks[0])
+print(marks[1])
+print(marks[2])
+  # print(marks[])
+
+if 7 in marks:
+  print("Yesss")
+else:
+  print("Nowee")
+
+list = [i for i in range(20)]
+print(list)
+list = [i for i in range(20) if i%4==0]
+print(list)
