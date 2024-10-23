d3f7934b061cae5d5509729126a89f8a77ac1ce7
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-23 20:16:27 +0500

    Create Day13.py

---
+++ Day13.py
@@ -0,0 +1,6 @@
+# f-strings in Python
+invitation = ("My name is {} and I am form {}")
+name = "Muhammad Ismail"
+country = "Pakistan"
+print(invitation.format(name,country))
+print(f"My name is {name} and I am from {country}")
