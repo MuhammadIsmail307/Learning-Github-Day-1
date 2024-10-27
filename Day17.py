30b1d3ba0d6ae0375d947d74005db7182dcd4d82
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-27 14:50:07 +0500

    Create Day17.py

---
+++ Day17.py
@@ -0,0 +1,17 @@
+# Set Methods
+# Union
+# intersection
+# Symmetric difference
+# difference
+# isdisjoint()
+# issuperset()
+# issubset()
+# .add()
+# .remove()
+# .discard()
+# .pop()  set me se value nikalne k lia use hota ha.
+# .clear()
+s1 = {11,2, 4, 5, 6,}
+s2 = {11, 55, 9,}
+s1.update(s2)
+print(s1.union(s2))
