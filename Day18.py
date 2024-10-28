5afd54fa14faa2abe107129e32029d27283c3439
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-28 17:58:56 +0500

    Create Day18.py

---
+++ Day18.py
@@ -0,0 +1,20 @@
+# dictionaries
+#  .get()
+#  .keys()
+dic = {
+    1 : "Ismail",
+    2 : "Najeeb",
+    3 : "Nisa",
+    4 : "Iqra",
+    5 : "Mama",
+    6 : "Papa",
+    7 : "Rehan",
+    8 : "Noman",
+    9 : "Uncle",
+    10 : "Najeeb",
+    11 : "Secret"
+}
+
+print(dic[1])
+print(dic[11])
+print(dic[5])
