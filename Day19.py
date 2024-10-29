f4a843fe111f5a3685cf17e9588c84b94a966712
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-29 15:58:42 +0500

    Create Day19.py

---
+++ Day19.py
@@ -0,0 +1,10 @@
+# Class 34
+# Dictionary Methods
+# .pop()
+# .popitem   ye dic ki last item ko del kr de ga.
+# .del()
+ep1 = {1: "ismail", 2:"najeeb", 3:"Ayesha"}
+ep2 = {4: "Iqra", 5:"M.Q", 6:"Noman"}
+
+ep1.update(ep2)
+print(ep1)
