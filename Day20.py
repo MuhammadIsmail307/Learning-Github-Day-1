b49cb8d30f0cd031bf96a2c7360c23a7f6d72bb8
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-10-30 13:05:11 +0500

    Create Day20.py

---
+++ Day20.py
@@ -0,0 +1,20 @@
+# class 35
+# Can we use For loop with Else Statements.
+
+for i in range(7):
+    print(i)
+    # print(++i)
+else :
+    print("Sorry No Iteration here")
+
+
+
+# [] means empty list
+for i in []:   # [] means empty list
+    print(i)
+    # print(++i)                                        # Use of else in for loop
+else :
+    print("Sorry No Iteration here")
+
+
+# You can also use while loop with else statement
