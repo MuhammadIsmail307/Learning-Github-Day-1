5966bf42718f914fc6c7bc343945c1ed93b2b8ce
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-11-06 15:08:09 +0500

    Create Day27.py

---
+++ Day27.py
@@ -0,0 +1,7 @@
+# Enumerate Function in python
+marks = [97, 88, 99, 87, 100]
+
+for index, mark in enumerate(marks):
+    print(mark)
+    if (index == 2):
+     print("Ismail you can do whatever you want")
