3d6f3fc25cb094ab8b5e7825f928e26a8f55628f
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-11-08 18:12:15 +0500

    Create Day29.py

---
+++ Day29.py
@@ -0,0 +1,11 @@
+# How to use import function  in python
+# import pandas
+# import math
+# import math as m
+# pandas.read_csv()
+
+
+# How to check all functions in a library
+
+import math
+print(dir(math))
