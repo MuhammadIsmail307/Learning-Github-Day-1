7c32638d989e8c1a89cb188b8a027453bdf9b5db
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-11-19 17:50:26 +0500

    Create Day40.py

---
+++ Day40.py
@@ -0,0 +1,16 @@
+# Snake water gun game in python 
+
+# Lecture 55 of Python Codewithharry
+
+
+# 1 st take input from user that will be snake or gun or water
+
+
+          S  G  W
+          0  1  2
+
+    S 0   D  W  L
+ 
+    G 1   L  D  W
+
+    W 2   W  L  D
