a89e9f78532c40dd26256b4d0eb77f81346d7b8e
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-11-20 18:02:06 +0500

    Create Day41.py

---
+++ Day41.py
@@ -0,0 +1,16 @@
+# OOPs in Python
+
+# OOPs stands for Object oriented programming
+
+# Lecture 56
+
+ 
+Railyform [ Class ] Blueprint
+
+Ismail 1 --> Isamil 1 ka form --> Object
+Ismail 2 --> Isamil 2 ka form --> Object
+Ismail 3 --> Isamil 3 ka form --> Object
+
+
+
+# Inheritance --> Making your code by using already existed code only by modifying that code
