a99f022f0d2a1e3ede2577d017130f37111c73cc
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-11-28 18:11:59 +0500

    Create Day49.py

---
+++ Day49.py
@@ -0,0 +1,14 @@
+# Exercise 6 - Library Management System in Python
+
+
+# lecture 64
+
+
+# I have to make a class library
+
+a =''' Write a library class with number of books and books
+ as two instance variables. Write a program to create 
+ a library from this library class and show how you can
+ print all books,add a book and get the number of 
+ books using different methods. Show that your program
+ does not persist the books after the program is stopped!'''
