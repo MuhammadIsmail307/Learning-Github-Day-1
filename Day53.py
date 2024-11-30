b90ce0c93be9eb0e3d0d5a1158b4f553e4668867
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-11-30 11:52:42 +0500

    Create Day53.py

---
+++ Day53.py
@@ -0,0 +1,18 @@
+# Exercise 7 - Clear the Clutter in python
+
+# Lecture 68
+
+
+b = '''Write a progra, to clear the clutter inside a 
+       folder in your computer. You should use OS Module
+       to rename all the png images from 1.png all the way 
+       till n.png where n is the number of png files in
+       that folder. Do the same for other file formats.
+       
+       For Example: 
+       
+       msg.png --> 1.png
+       whatsapp.png --> 2.png
+       ss.png --> 3.png  '''
+
+print(b)
