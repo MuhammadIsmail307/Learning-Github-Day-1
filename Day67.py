2f636f682776b3fb78d8e4cb97b23e88d286a786
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-12-12 18:16:01 +0500

    Create Day67.py

---
+++ Day67.py
@@ -0,0 +1,23 @@
+# Exercise 9 - Shoutouts to Everyone
+
+
+# Lecture 83
+
+
+# Write a program to pronounce list of names using win32 API.
+
+
+# if you are given a list 'l' as follows :
+
+l = ["ismail ", "Najeeb", "Rehan"]
+
+
+# Your program should pronounce :
+
+# Shoutout to ismail
+# Shoutout to Najeeb
+# Shoutout to Rehan
+
+
+
+# Sapivoice windows python 
