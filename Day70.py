17a8591e0d3b74359b5c9cf6010cdbdb9bcd9e3a
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-12-15 16:59:38 +0500

    Create Day70.py

---
+++ Day70.py
@@ -0,0 +1,21 @@
+# Walrus Operator in Python
+
+
+# Lecture 86
+
+a = True
+
+print(a=False)
+
+# output = Error
+
+a = True
+
+print(a:=False)
+
+# ==================================
+
+
+numbers = [1,2,3,4,5]
+while (n:= len(numbers)) > 0 :
+    print(numbers.pop())
