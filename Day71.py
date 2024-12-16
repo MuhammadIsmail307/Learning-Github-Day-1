b7b5919a5a018caa482524360ed9aba922c6e2a5
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-12-16 19:58:12 +0500

    Create Day71.py

---
+++ Day71.py
@@ -0,0 +1,22 @@
+# Shutil Module in Python
+
+
+# Lecture 87
+
+
+# shutil ---> is a built in model
+
+
+# some related functions
+
+import shutil
+
+# shutil.copy
+
+# shutil.copytree
+
+# shutil.copy2
+
+# shutil.move
+
+# shutil.rmtree
